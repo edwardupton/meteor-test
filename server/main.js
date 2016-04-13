54582866c60e80c56c361db67ee0890429c27ccb
edwardupton <edward@teachable.co.uk>
2016-04-13 17:24:23 +0300

    Meteor Jasmine test

---
+++ server/main.js
@@ -0,0 +1,9 @@
+import { Meteor } from 'meteor/meteor';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+});
+
+multiply = (x,y) => {
+	return x * y
+}
---
+++ server/main.test.js
@@ -0,0 +1,5 @@
+describe('multiply',() => {
+	it('returns 4 when given x=2, y=2',() => {
+		expect(multiply(2,2)).toEqual(4)
+	})
+})
